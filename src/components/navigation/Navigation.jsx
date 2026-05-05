bd9fff192e54a90c0489d990c1fd21009152ed3c
Cherie Chan <Cherie.Chan@kmart.com.au>
2026-05-05 10:50:52 +1000

    Fix nav

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,20 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import c from './navigation.module.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import c from "./navigation.module.scss";
 
 const Navigation = () => {
   return (
     <nav className={c.navigation}>
-      <div className={c.container}>
-        <Link to="/" className={c.logo}>
-          Cherie
-        </Link>
-        <Link to="/about" className={c.aboutLink}>
-          About
-        </Link>
-      </div>
+      <div className={c.container}>Cherie Articles</div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
